92ff3a779a097b09dfb9cdab3a66cbcef4b28e1f
Monk <cryptomonk12@gmail.com>
2022-09-12 05:48:00 -0400

    initial commit

---
+++ API.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+const server = app.listen(3000, console.log('Proxy server is running on port 3000'));
+const crypto = require('crypto');
+const connectUtils =require("./connectUtils");
+
+// Create a private key
+const { privateKey } = crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+});
+
+
+// API for get all data with paramter token name ex: http://127.0.0.1:3000/MATICUSDT
+app.get('/:name', async (req, res) => {
+  try {
+    const { name } = req.params;
+    const resp =  await connectUtils.findByName(name);  
+    res.status(200).json(resp);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+
+// API for get signed data with paramter multi token name split with coma and date  ex: http://127.0.0.1:3000/MATICUSDT,BNBUSDT\2022-09-11T20:54:30.613Z
+app.get('/:names/:time_', async (req, res) => {
+  try {
+    const { names ,time_} = req.params;
+    const namelist = names.split(",");
+    const resp =  await connectUtils.findByNameList(namelist,time_); 
+    
+    // Convert Stringified json data to buffer 
+    const data = Buffer.from( JSON.stringify(resp) );
+  
+    // Sign the data and returned signature in buffer 
+    const sign = crypto.sign("SHA256", data , privateKey);
+      
+    // Convert returned buffer to base64
+    const signature = sign.toString('base64');
+
+    res.status(200).json(signature);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+
+// API for verifying signed data  
+app.get('/:signature/:names/:time_', async (req, res) => {
+  try {
+    const { signature,names ,time_} = req.params;
+    const namelist = names.split(",");
+    const resp =  await connectUtils.findByNameList(namelist,time_); 
+    
+    const isVerified = crypto.verify(
+      "sha256",
+      Buffer.from(resp),
+      {
+        key: publicKey,
+        padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
+      },
+      signature
+    );
+    
+    // isVerified should be `true` if the signature is valid
+    console.log("signature verified: ", isVerified);
+
+    res.status(200).json(isVerified);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
---
+++ README.md
@@ -0,0 +1,28 @@
+# Backend Developer Challenge
+ 
+
+Requirements For Initial Setup : 
+
+   Install NodeJS
+
+Setting Up:
+
+1. Clone/Download the Repository
+
+2. Install Dependencies:
+  
+   npm install 
+
+3. Run fetchData : 
+
+   node fetchData.js
+
+4. Run API's :
+
+   node API.js
+
+
+
+
+
+
